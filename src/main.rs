bd5166e73cfddab2429a140798d1192fc6434338
Nick Hemsley <nick.hems@gmail.com>
2020-08-13 13:07:32 +1000

    working well

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,24 @@ extern crate three;
 
 // use cgmath::prelude::*;
 use three::Object;
+use cgmath::{Angle, Decomposed, One, Quaternion, Rad, Rotation3, Transform, Vector3};
+
 // use mint::*;
 pub mod packing;
 mod sweeping_iterator;
 
 fn main() {
-    let max_points: u32 = 100;
+    let max_points: u32 = 500;
 
     let mut win = three::Window::new("Fibonacci sphere packing");
     let cam = win.factory.perspective_camera(75.0, 1.0 .. 50.0);
     cam.set_position([0.0, 0.0, 5.0]);
+
+    let mut controls = three::controls::Orbit::builder(&cam)
+        .position([0.0, 3.0, -1.0])
+        .target([0.0, 0.0, -1.0])
+        .up([0.0, 0.0, -1.0])
+        .build();
 
     let light = win.factory.point_light(0xffffff, 1.0);
     light.set_position([0.0, -10.0, 10.0]);
@@ -23,36 +31,58 @@ fn main() {
 
     // let num_points = sweep.next().unwrap();
     // let num_points = sweep.next().unwrap();
-    let mut num_points = 5;
+    let mut num_points: u32 = 5;
     let mut spheres: Vec<three::Mesh> = vec!{};
+
+    let group: three::Group = win.factory.group();
+    let rot = Quaternion::from_axis_angle(Vector3::unit_y(), Rad::turn_div_4());
+    let mut changed = true;
+    group.set_orientation(rot);
+    group.set_scale(2.0);
+    
+    win.scene.add(&group);
+
     while win.update() && !win.input.hit(three::KEY_ESCAPE) {
         // println!("{}", num_points);
-        let points: Vec<mint::Point3<f32>> = packing::points_on_sphere_fib(num_points as u32);
+        controls.update(&win.input);
 
-        // let sync = win.scene.sync_guard();
-        if spheres.len() > 0 {
-            spheres.iter().for_each(|sphere| win.scene.remove(sphere))
+        let points: Vec<mint::Point3<f32>> = packing::points_on_sphere_fib(num_points, 0.7);
+        // println!("{}", num_points);
+
+        if changed {
+            println!("changed, resetting group");
+            // let sync = win.scene.sync_guard();
+            if spheres.len() > 0 {
+                spheres.iter().for_each(|sphere| group.remove(sphere))
+            }
+
+            spheres = points.iter().map(|&point| {
+                let geometry = three::Geometry::uv_sphere(0.05, 4, 4);
+                let material = three::material::Lambert { color: 0x80ff80, flat: false };
+        
+                let mesh = win.factory.mesh(geometry, material.clone());
+                // println!("{:#?}", mesh);
+                mesh.set_position(point);
+                return mesh;
+            }).collect::<Vec<three::Mesh>>();
+        
+            spheres.iter().for_each(|sphere| group.add(sphere));
         }
-
-        spheres = points.iter().map(|&point| {
-            let geometry = three::Geometry::uv_sphere(0.1, 10, 10);
-            let material = three::material::Lambert { color: 0x80ff80, flat: false};
-    
-            let mesh = win.factory.mesh(geometry, material.clone());
-            // println!("{:#?}", mesh);
-            mesh.set_position(point);
-            return mesh;
-        }).collect::<Vec<three::Mesh>>();
-    
-        spheres.iter().for_each(|sphere| win.scene.add(&sphere));
     
         if let Some(diff) = win.input.timed(three::AXIS_LEFT_RIGHT) {
-            println!("{}", diff);
+            // println!("{}", diff);
             if diff > 0.0 {
                 num_points = (num_points + 1).min(max_points);
+                changed = true;
             } else if diff < 0.0 && num_points > 0 {
                 num_points = (num_points - 1).max(0);
+                changed = true;
+            } else {
+                changed = false;
             }
+        } else {
+            // println!("changing = false");
+            if changed == true { changed = false};
         }
         win.render(&cam);
     }
--- src/packing.rs
+++ src/packing.rs
@@ -3,44 +3,48 @@ extern crate mint;
 
 
 pub fn generate_epsilon(n: u32) -> f32 {
-  let epsilon: f32;
-  if n >= 600000 {
-    epsilon = 214.0;
-  } else if n>= 400000 {
-    epsilon = 75.0
-  } else if  n>= 11000 {
-    epsilon = 27.0
-  } else if  n>= 890 {
-    epsilon = 10.0
-  } else if  n>= 177 {
-    epsilon = 3.33
-  } else if  n>= 24 {
-    epsilon = 1.33
-  } else {
-    epsilon = 0.33
-  };
+    return  0.36;
 
-  return epsilon;
+//     let epsilon: f32;
+//   if n >= 600000 {
+//     epsilon = 214.0;
+//   } else if n>= 400000 {
+//     epsilon = 75.0
+//   } else if  n>= 11000 {
+//     epsilon = 27.0
+//   } else if  n>= 890 {
+//     epsilon = 10.0
+//   } else if  n>= 177 {
+//     epsilon = 3.33
+//   } else if  n>= 24 {
+//     epsilon = 1.33
+//   } else {
+//     epsilon = 0.33
+//   };
+
+
 
 }
 
-pub fn points_on_sphere_fib(n: u32) -> Vec<mint::Point3<f32>> {
+pub fn points_on_sphere_fib(n: u32, ratio: f32) -> Vec<mint::Point3<f32>> {
 
-    let _epsilon = generate_epsilon(n);
+    let to = 1 + (n.clone() as f32 * (1.0 / ratio)) as u32;
+    let skip = (to as f32 * ratio) as u32;
+    // println!("n.original: {} ratio: {} to: {} skip: {}", n, ratio, to, skip);
+    let _epsilon = generate_epsilon(to - skip);
     // println!("{}", _epsilon);
     let five: f32 = 5.0;
     let golden_ratio: f32 = (1.0 + five.powf(0.5)) / 2.0;
     // println!("{}", golden_ratio);
 
-    let range: Vec<u32> = (0..n).collect();
-
+    let range: Vec<u32> = (0..skip).collect();
 
     // thetas = 2 * pi * i / goldenRatio
     let thetas: Vec<f32> = range.iter().map(|i: &u32| { 2.0 as f32 * std::f32::consts::PI * (*i as f32) / golden_ratio}).collect::<Vec<f32>>();
 
     let phis: Vec<f32> = range.iter().map(|i: &u32| {
         //  phi_a = arccos(1 - 2*(i+epsilon)/(n-1+2*epsilon))
-        let acos_of: f32 = 1.0 - (2.0 * ((*i as f32) + _epsilon) / (n as f32 - 1.0 + 2.0 * _epsilon));
+        let acos_of: f32 = 1.0 - (2.0 * ((*i as f32) + _epsilon) / (to as f32 - 1.0 + 2.0 * _epsilon));
         return acos_of.acos();
     }).collect::<Vec<f32>>();
     // println!("{:#?}", thetas);
