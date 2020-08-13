eca47d59698c25bb6d729d9bf06284af493a70d2
Nick Hemsley <nick.hems@gmail.com>
2020-08-13 14:42:57 +1000

    speed updates

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ pub mod packing;
 mod sweeping_iterator;
 
 fn main() {
-    let max_points: u32 = 500;
+    let max_points: u32 = 5000;
 
     let mut win = three::Window::new("Fibonacci sphere packing");
     let cam = win.factory.perspective_camera(75.0, 1.0 .. 50.0);
@@ -24,7 +24,7 @@ fn main() {
         .build();
 
     let light = win.factory.point_light(0xffffff, 1.0);
-    light.set_position([0.0, -10.0, 10.0]);
+    light.set_position([10.0, 10.0, 10.0]);
     win.scene.add(&light);
 
     // let mut sweep = sweeping_iterator::SweepingIterator::new(max_points as i32);
@@ -41,26 +41,33 @@ fn main() {
     group.set_scale(2.0);
     
     win.scene.add(&group);
+    let geometry = three::Geometry::uv_sphere(0.05, 8, 8);
+    let upload_geometry = win.factory.upload_geometry(geometry);
+
 
     while win.update() && !win.input.hit(three::KEY_ESCAPE) {
         // println!("{}", num_points);
         controls.update(&win.input);
 
-        let points: Vec<mint::Point3<f32>> = packing::points_on_sphere_fib(num_points, 0.7);
+        let points: Vec<mint::Point3<f32>> = packing::points_on_sphere_fib(num_points, 0.9);
         // println!("{}", num_points);
 
         if changed {
-            println!("changed, resetting group");
+            println!("changed, resetting group: n: {}", num_points);
             // let sync = win.scene.sync_guard();
             if spheres.len() > 0 {
                 spheres.iter().for_each(|sphere| group.remove(sphere))
             }
 
+            let mut i: u32 = 0;
+
             spheres = points.iter().map(|&point| {
-                let geometry = three::Geometry::uv_sphere(0.05, 4, 4);
-                let material = three::material::Lambert { color: 0x80ff80, flat: false };
+                i += 1;
+                let mut material = three::material::Lambert { color: 0x80ff80, flat: false };
+                if i % 20 == 0 { material = three::material::Lambert { color: 0xff0080, flat: false }; }
+                let mesh = win.factory.create_instanced_mesh(&upload_geometry, material.clone());
         
-                let mesh = win.factory.mesh(geometry, material.clone());
+                // let mesh = win.factory.mesh(geometry, material.clone());
                 // println!("{:#?}", mesh);
                 mesh.set_position(point);
                 return mesh;
@@ -68,18 +75,23 @@ fn main() {
         
             spheres.iter().for_each(|sphere| group.add(sphere));
         }
+
+        let change_per_diff = 30;
     
         if let Some(diff) = win.input.timed(three::AXIS_LEFT_RIGHT) {
-            // println!("{}", diff);
-            if diff > 0.0 {
-                num_points = (num_points + 1).min(max_points);
+            if diff > 0.0  && num_points < max_points {
+                num_points = (num_points + change_per_diff).min(max_points);
                 changed = true;
             } else if diff < 0.0 && num_points > 0 {
-                num_points = (num_points - 1).max(0);
+                num_points = (num_points as i32 - change_per_diff as i32).max(0) as u32;
+                println!("{}", num_points);
                 changed = true;
             } else {
                 changed = false;
             }
+            // if changed == true {
+            //     println!("{}", diff);
+            // }
         } else {
             // println!("changing = false");
             if changed == true { changed = false};
